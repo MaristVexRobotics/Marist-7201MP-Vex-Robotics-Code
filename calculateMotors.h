2da220a4186a8d32227f70af9573aec443e7b1b3
kradKraemz <kradKraemz@users.noreply.github.com>
2014-09-18 10:01:49 -0400

    Math almost fixed. it goes right instead of left...

--- calculateMotors.h
+++ calculateMotors.h
@@ -1,11 +1,17 @@
 
 void calcMotorValues() {
 
-	int actualFWD = rcDriveForward * cosDegrees(-gyroValue);
-	int actualLFT = rcDriveStrafe * sinDegrees(-gyroValue);
+	int actualFWD = rcDriveForward * cosDegrees(gyroValue) + rcDriveStrafe * cosDegrees(90 - gyroValue);
+	int actualLFT = rcDriveForward  * sinDegrees(gyroValue) + rcDriveStrafe  * sinDegrees(90 - gyroValue);
+
+
+	/*
+	RobotY = AbsoluteY * cos(-angle) + AbsoluteX * cos(90 + angle);
+	RobotX = AbsoluteY * sin(-angle) + AbsoluteX * sin(90 + angle);
+	*/
 
 	leftFrontDrivePower = actualFWD + actualLFT + rcDriveTurn;
 	leftBackDrivePower = actualFWD - actualLFT + rcDriveTurn;
-	rightFrontDrivePower = actualFWD - actualLFT + rcDriveTurn;
-	rightBackDrivePower = actualFWD + actualLFT + rcDriveTurn;
+	rightFrontDrivePower = actualFWD - actualLFT - rcDriveTurn;
+	rightBackDrivePower = actualFWD + actualLFT - rcDriveTurn;
 }
