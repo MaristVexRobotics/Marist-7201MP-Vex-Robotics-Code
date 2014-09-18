1084ffc9f7c86f4e77d6dcd36becc57bd5a89eb6
kradKraemz <kradKraemz@users.noreply.github.com>
2014-09-18 10:29:48 -0400

    Fixed holonomic relative drive. Works Perfectly. Todo, add code to calibrate directions

--- calculateMotors.h
+++ calculateMotors.h
@@ -1,17 +1,24 @@
 
 void calcMotorValues() {
 
-	int actualFWD = rcDriveForward * cosDegrees(gyroValue) + rcDriveStrafe * cosDegrees(90 - gyroValue);
-	int actualLFT = rcDriveForward  * sinDegrees(gyroValue) + rcDriveStrafe  * sinDegrees(90 - gyroValue);
+
+	int actualFWD = rcDriveForward * cosDegrees(gyroValue) + rcDriveStrafe * cosDegrees(90 + gyroValue);
+	int actualLFT = rcDriveForward * sinDegrees(gyroValue) + rcDriveStrafe * sinDegrees(90 + gyroValue);
 
 
 	/*
-	RobotY = AbsoluteY * cos(-angle) + AbsoluteX * cos(90 + angle);
-	RobotX = AbsoluteY * sin(-angle) + AbsoluteX * sin(90 + angle);
+	int actualFWD = rcDriveForward;
+	int actualLFT = rcDriveStrafe;
 	*/
 
-	leftFrontDrivePower = actualFWD + actualLFT + rcDriveTurn;
-	leftBackDrivePower = actualFWD - actualLFT + rcDriveTurn;
-	rightFrontDrivePower = actualFWD - actualLFT - rcDriveTurn;
-	rightBackDrivePower = actualFWD + actualLFT - rcDriveTurn;
+
+	/*
+	Fwd = Positive
+	Right = Positive
+	*/
+
+	leftFrontDrivePower = actualFWD - actualLFT + rcDriveTurn;
+	leftBackDrivePower = actualFWD + actualLFT + rcDriveTurn;
+	rightFrontDrivePower = actualFWD + actualLFT - rcDriveTurn;
+	rightBackDrivePower = actualFWD - actualLFT - rcDriveTurn;
 }
