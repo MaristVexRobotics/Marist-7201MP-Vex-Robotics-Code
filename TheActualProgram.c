24a57b8660ba2a49050bc5cf86c84751c875aa87
kradKraemz <kradKraemz@users.noreply.github.com>
2014-09-20 12:09:35 -0400

    Purtified The version info

--- TheActualProgram.c
+++ TheActualProgram.c
@@ -33,15 +33,15 @@
 
 
 /*****************************************************\
-|		Code by Konrad Kraemer														|
-|																											|
-|		Current Version 3.0 BETA													|
-|																											|
-|		Modular code to ease testing on VirtualWorlds.		|
-|		Autonomous code records itself using the 					|
-|		writeStream() function.														|
-|																											|
-|																											|
+|   Code by Konrad Kraemer                            |
+|                                                     |
+|   Current Version 3.0 BETA                          |
+|                                                     |
+|   Modular code to ease testing on VirtualWorlds.    |
+|   Autonomous code records itself using the          |
+|   writeStream() function.                           |
+|                                                     |
+|                                                     |
 | 3.0   Rewrite - codebase completely rewritten to    |
 |           acommodate the mechanum wheels, control   |
 |           the scissor lift, and redo the autonomous |
@@ -52,86 +52,86 @@
 |           driver, regardless of the rotation of the |
 |           robot.                                    |
 |                                                     |
-| 2.0.8.1 Tweak - correction value changed to .5 to   |
-|						make the autonomous even less jumpy				|
-|																											|
-| 2.0.8 Modification - removed the lurching and 			|
-|						anti-tippingcode because our drivers			|
-|						thought it limited our mobility						|
-|																											|
-|	2.0.7 Feature Replacement - we decided that we need	|
-|						full power, so we are dropping the 2.0.5	|
-|						changes, and instead tweaking the accel		|
-|																											|
-|	2.0.6	Feature- software speed limiting to prevent		|
-|						the robot from lurching forward if arm 		|
-|						is fully up. This previously caused the		|
-|						robot to flip.														|
-|																											|
-| 2.0.5	Feature- software arm limiting to prevent the	|
-|						arm from exceding a certain height and		|
-|						flipping the robot. Also, this code turns	|
-|						off the automatic arm	power which would		|
-|						keep it level.														|
-|																											|
-|	2.0.4.1 Tweak - ticks to give up changed to 500			|
-|						ticks. Now it doesn't take soo long				|
-|						to move on. Also, correction value				|
-|						changed to 1 to make it less jumpy.				|
-|																											|
-|	2.0.4		BugFix - told robot to give up after				|
-|						1000 ticks. Robot used to try to					|
-|						complete a command indefinitely, if				|
-|						it hit a wall, it would just sit there		|
-|						and do nuthin, so now it gives up.				|
-| 																										|
-|	2.0.3.2	Tweak - deadzone changed to 27. 						|
-|						Maximum speed	becomes 100 								|
-|					JANUARY 6, 2014															|
-|																											|
-|	2.0.3.1	Bugfix - correction code wasn't working.		|
-|						motor values went to -3000s. Problem was	|
-|						that I accidentaly read the left encoder	|
-|						instead of the right encoder 							|
-|							JANUARY 6, 2014													|
-|																											|
-|	2.0.3	Bugfix - some motors were having problems			|
-|					catching up with the others, added code to  |
-|					set the max speed for motors during					|
-|					recording to 127 MINUS the value of 				|
-|					variable deadzone (max 122).								|
-|						 JANUARY 6, 2014													|
-|																											|
-|	2.0.2	Bugfix - same problem as 2.0.1, I solved it		|
-|					by having the code wait any encoder reaches	|
-|					its predefined target for that cycle.				|
-|						JANUARY 5, 2014														|
-|																											|
-|	2.0.1	Bugfix - since the recording code takes more	|
-|					time to execute than the autonomous, the		|
-|					autonomous code ends up running a tad too		|
-|					quickly. I started fixing this by forcing 	|
-|					certain parts of the code to execute at			|
-|					a certain time, however I abandoned this		|
-|					approach and went with another (2.0.2)			|
-|							JANUARY 4, 2014													|
-|																											|
-|	2.0		Complete rewrite - modular code to aid 				|
-|				testing in VirtualWorlds and make the code		|
-|				more legible. Autonoumous recording now				|
-|				supports full control with thumbsticks				|
-|							December 28, 2013												|
-|																											|
-|	1.2		Rewrote Autonomous - Working	- FALL 2013			|
-|					Codes itself, uses D-pad controls						|
-|					(fwd,back,turnLeft,turnRight)								|
-|																											|
-|	1.1		Added self-writing Autonomous									|
-|					Was unfunctional, witten early 2013					|
-|																											|
-|	1.0		Original Version Of remote code	Late 2012			|
-|																											|
-|																											|
+| 2.0.8.1 Tweak- correction value changed to .5 to    |
+|           make the autonomous even less jumpy       |
+|                                                     |
+| 2.0.8 Modification- removed the lurching and        |
+|           anti-tippingcode because our drivers      |
+|           thought it limited our mobility           |
+|                                                     |
+| 2.0.7 Feature Replacement- we decided that we need  |
+|           full power, so we are dropping the 2.0.5  |
+|           changes, and instead tweaking the accel   |
+|                                                     |
+| 2.0.6	Feature- software speed limiting to prevent   |
+|           the robot from lurching forward if arm    |
+|           is fully up. This previously caused the   |
+|           robot to flip.                            |
+|                                                     |
+| 2.0.5	Feature- software arm limiting to prevent the |
+|           arm from exceding a certain height and    |
+|           flipping the robot. Also, this code turns |
+|           off the automatic arm power which would   |
+|           keep it level.                            |
+|                                                     |
+| 2.0.4.1 Tweak- ticks to give up changed to 500      |
+|           ticks. Now it doesn't take soo long       |
+|           to move on. Also, correction value        |
+|           changed to 1 to make it less jumpy.       |
+|                                                     |
+| 2.0.4	BugFix- told robot to give up after           |
+|           1000 ticks. Robot used to try to          |
+|           complete a command indefinitely, if       |
+|           it hit a wall, it would just sit there    |
+|           and do nuthin, so now it gives up.        |
+|                                                     |
+| 2.0.3.2 Tweak- deadzone changed to 27.              |
+|           Maximum speed becomes 100                 |
+|           JANUARY 6, 2014                           |
+|                                                     |
+| 2.0.3.1 Bugfix- correction code wasn't working.     |
+|           motor values went to -3000s. Problem was  |
+|           that I accidentaly read the left encoder  |
+|           instead of the right encoder              |
+|           JANUARY 6, 2014                           |
+|                                                     |
+| 2.0.3	Bugfix- some motors were having problems      |
+|           catching up with the others, added code   |
+|           to set the max speed for motors during    |
+|           recording to 127 MINUS the value of       |
+|           variable deadzone (max 122).              |
+|           JANUARY 6, 2014                           |
+|                                                     |
+| 2.0.2	Bugfix- same problem as 2.0.1, I solved it    |
+|           by having the code wait until any encoder |
+|           reaches its predefined target for that    |
+|           cycle.     JANUARY 5, 2014                |
+|                                                     |
+| 2.0.1 Bugfix- since the recording code takes more   |
+|           time to execute than the autonomous, the  |
+|           autonomous code ends up running a tad too |
+|           quickly. I started fixing this by forcing |
+|           certain parts of the code to execute at   |
+|           a certain time, however I abandoned this  |
+|           approach and went with another (2.0.2)    |
+|           JANUARY 4, 2014                           |
+|                                                     |
+| 2.0  Complete rewrite- modular code to aid          |
+|           testing in VirtualWorlds and make it      |
+|           more legible. Autonoumous recording now   |
+|           supports full control with thumbsticks    |
+|           December 28, 2013                         |
+|                                                     |
+| 1.2 Rewrote Autonomous- Working - FALL 2013         |
+|           Codes itself, uses D-pad controls         |
+|           (fwd,back,turnLeft,turnRight)             |
+|                                                     |
+| 1.1 Added self-writing Autonomous                   |
+|           Was not functional, witten early 2013     |
+|                                                     |
+| 1.0 Original Version Of remote code - Late 2012     |
+|                                                     |
+|                                                     |
 \*****************************************************/
 
 //=====================
