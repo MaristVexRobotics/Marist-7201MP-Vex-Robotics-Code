9635c26708c56f3eb4ba3c6f9beae0b930f7ac92
ItsDaSpecialK <ItsDaSpecialK@users.noreply.github.com>
2015-01-13 12:18:24 -0500

    Beautify Robot.h

--- Robot.h
+++ Robot.h
@@ -1,66 +1,64 @@
 void RunRobot(){
-	//read sensor values and put them into the variables
-	leftFrontEncoder  = nMotorEncoder[leftFront];
-	rightFrontEncoder = nMotorEncoder[rightFront];
-	leftBackEncoder   = nMotorEncoder[leftBack];
-	rightBackEncoder  = nMotorEncoder[rightBack];
+  //read sensor values and put them into the variables
+  leftFrontEncoder  = nMotorEncoder[leftFront];
+  rightFrontEncoder = nMotorEncoder[rightFront];
+  leftBackEncoder   = nMotorEncoder[leftBack];
+  rightBackEncoder  = nMotorEncoder[rightBack];
+  
+  leftArmDegrees  = SensorValue[leftScissorRot];// - POTENTIOMETER_DIFF;
+  rightArmDegrees = SensorValue[leftScissorRot];//SensorValue[rightScissorRot];
+  beltSensorVal   = nMotorEncoder[rightBelt];
 
-	leftArmDegrees  = SensorValue[leftScissorRot];// - POTENTIOMETER_DIFF;
-	rightArmDegrees = SensorValue[leftScissorRot];//SensorValue[rightScissorRot];
-	beltSensorVal   = nMotorEncoder[rightBelt];
+  if (gyroOverride)//3.0.1
+    gyroValue = 0;
+  else
+    gyroValue = SensorValue[gyro]/10;
 
-	if (gyroOverride)//3.0.1
-		gyroValue = 0;
-	else
-		gyroValue = SensorValue[gyro]/10;
-
-	motor[leftFront]  = leftFrontDrivePower * EFL;
-	motor[leftBack]   = leftBackDrivePower * EBL;
-	motor[rightFront] = rightFrontDrivePower * EFR;
-	motor[rightBack]  = rightBackDrivePower * EBR;
+  motor[leftFront]  = leftFrontDrivePower * EFL;
+  motor[leftBack]   = leftBackDrivePower * EBL;
+  motor[rightFront] = rightFrontDrivePower * EFR;
+  motor[rightBack]  = rightBackDrivePower * EBR;
 
 
-	motor[leftScissor1]  = armPower;
-	motor[leftScissor2]  = armPower;
-	motor[rightScissor1] = armPower;
-	motor[rightScissor2] = armPower;
+  motor[leftScissor1]  = armPower;
+  motor[leftScissor2]  = armPower;
+  motor[rightScissor1] = armPower;
+  motor[rightScissor2] = armPower;
 
-	int diff = 0;//rightArmDegrees - leftArmDegrees;
-	if (armPower > 0) {
-		if (leftArmDegrees > rightArmDegrees) {
-			motor[leftScissor1] += diff;
-			motor[leftScissor2] += diff;
-		}
-		else /*if (leftArmDegrees < rightArmDegrees)*/ {
-			motor[rightScissor1] -= diff;
-			motor[rightScissor2] -= diff;
-		}
-	} else /*if (scissorPower < 0)*/ {
-		if (leftArmDegrees > rightArmDegrees) {
-			motor[rightScissor1] -= diff;
-			motor[rightScissor2] -= diff;
-		}
-		else /*if (leftArmDegrees < rightArmDegrees)*/ {
-			motor[leftScissor1] += diff;
-			motor[leftScissor2] += diff;
-		}
-	}
+  int diff = 0;//rightArmDegrees - leftArmDegrees;
+  if (armPower > 0) {
+    if (leftArmDegrees > rightArmDegrees) {
+      motor[leftScissor1] += diff;
+      motor[leftScissor2] += diff;
+    } else /*if (leftArmDegrees < rightArmDegrees)*/ {
+      motor[rightScissor1] -= diff;
+      motor[rightScissor2] -= diff;
+    }
+  } else /*if (scissorPower < 0)*/ {
+    if (leftArmDegrees > rightArmDegrees) {
+      motor[rightScissor1] -= diff;
+      motor[rightScissor2] -= diff;
+    } else /*if (leftArmDegrees < rightArmDegrees)*/ {
+      motor[leftScissor1] += diff;
+      motor[leftScissor2] += diff;
+    }
+  }
 
 
 
-	motor[leftBelt] = beltPower;
-	motor[rightBelt] = beltPower;
+  motor[leftBelt] = beltPower;
+  motor[rightBelt] = beltPower;
 
 }
 
 void resetSensors() {
-	nMotorEncoder[leftFront]  = 0;
-	nMotorEncoder[rightFront] = 0;
-	nMotorEncoder[leftFront]  = 0;
-	nMotorEncoder[rightFront] = 0;
-	nMotorEncoder[leftBack]   = 0;
-	nMotorEncoder[rightBack]  = 0;
-	nMotorEncoder[rightBelt]  = 0;
-	SensorValue[rightScissorRot] = 0;
-	SensorValue[leftScissorRot]  = 0;
+  nMotorEncoder[leftFront]  = 0;
+  nMotorEncoder[rightFront] = 0;
+  nMotorEncoder[leftFront]  = 0;
+  nMotorEncoder[rightFront] = 0;
+  nMotorEncoder[leftBack]   = 0;
+  nMotorEncoder[rightBack]  = 0;
+  nMotorEncoder[rightBelt]  = 0;
+  SensorValue[rightScissorRot] = 0;
+  SensorValue[leftScissorRot]  = 0;
 }
