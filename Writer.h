3f95a16aa56b011cba132d03505277f3477c7f8f
ItsDaSpecialK <ItsDaSpecialK@users.noreply.github.com>
2015-01-13 13:58:26 -0500

    Beautify Writer.h

--- Writer.h
+++ Writer.h
@@ -1,7 +1,7 @@
 void writeStream() {
 
 
-	//----------------------
+  //----------------------
   if (leftBackDrivePower < -127 + DEADZONE)
     leftBackDrivePower = -127 + DEADZONE;
   else if (leftBackDrivePower > 127 - DEADZONE)
@@ -22,36 +22,36 @@ void writeStream() {
   else if (rightFrontDrivePower > 127 - DEADZONE)
     rightFrontDrivePower = 127 - DEADZONE;
 
-	if (armPower < -127 + DEADZONE)
-		armPower = -127 + DEADZONE;
-	else if (armPower > 127 - DEADZONE)
-		armPower = 127 - DEADZONE;
-	//----------------------
+  if (armPower < -127 + DEADZONE)
+    armPower = -127 + DEADZONE;
+  else if (armPower > 127 - DEADZONE)
+    armPower = 127 - DEADZONE;
+  //----------------------
 
 
-	// if there is something happening, write autonomous code
-	if (abs(leftBackDrivePower)  > 10 || abs(rightBackDrivePower)  > 10
+  // if there is something happening, write autonomous code
+  if (abs(leftBackDrivePower)  > 10 || abs(rightBackDrivePower)  > 10
    || abs(leftFrontDrivePower) > 10 || abs(rightFrontDrivePower) > 10
    || abs(armPower) > 10 || abs(beltPower) > 10
    || abs(leftFrontEncoder) > 10 || abs(rightFrontEncoder) > 10
    || abs(leftBackEncoder)  > 10 || abs(rightBackEncoder)  > 10
    || abs(leftArmDegrees)   > 10 || abs(rightArmDegrees)   > 10) {
 
-		writeDebugStream("Auton(%d, ", (leftFrontDrivePower));
-		writeDebugStream("%d, ", (leftBackDrivePower));
-		writeDebugStream("%d, ", (rightFrontDrivePower));
-		writeDebugStream("%d, ", (rightBackDrivePower));
-		writeDebugStream("%d, ", (armPower));
-		writeDebugStream("%d, ", (beltPower));
-		writeDebugStream("%d, ", (leftFrontEncoder));
-		writeDebugStream("%d, ", (leftBackEncoder));
-		writeDebugStream("%d, ", (rightFrontEncoder));
-		writeDebugStream("%d, ", (rightBackEncoder));
-		writeDebugStream("%d, ", (leftArmDegrees));
-		writeDebugStreamLine("%d);", (rightArmDegrees));
-		writeDebugStreamLine("RunRobot();");
-		writeDebugStreamLine("wait10Msec(%d);", (AUTO_TIME_INTERVAL));
+    writeDebugStream("Auton(%d, ", (leftFrontDrivePower));
+    writeDebugStream("%d, ", (leftBackDrivePower));
+    writeDebugStream("%d, ", (rightFrontDrivePower));
+    writeDebugStream("%d, ", (rightBackDrivePower));
+    writeDebugStream("%d, ", (armPower));
+    writeDebugStream("%d, ", (beltPower));
+    writeDebugStream("%d, ", (leftFrontEncoder));
+    writeDebugStream("%d, ", (leftBackEncoder));
+    writeDebugStream("%d, ", (rightFrontEncoder));
+    writeDebugStream("%d, ", (rightBackEncoder));
+    writeDebugStream("%d, ", (leftArmDegrees));
+    writeDebugStreamLine("%d);", (rightArmDegrees));
+    writeDebugStreamLine("RunRobot();");
+    writeDebugStreamLine("wait10Msec(%d);", (AUTO_TIME_INTERVAL));
 
-	}
-	wait10Msec(AUTO_TIME_INTERVAL);
+  }
+  wait10Msec(AUTO_TIME_INTERVAL);
 }
